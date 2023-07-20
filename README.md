0a0ee24ebc3c224df435acd3c72dc27d5ba59314
ntrupin <natrupin@gmail.com>
2023-07-20 17:58:13 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Noahvim
+
+My Neovim configuration.
---
+++ lua/plugins.lua
@@ -0,0 +1,31 @@
+-- plugins
+local plugins = {
+  -- appearance
+  { 
+    "sainnhe/sonokai", -- colorscheme
+    config = function()
+      vim.cmd("colorscheme sonokai")
+    end
+  },
+
+  -- misc dependencies
+
+  -- better syntax highlighting
+  { "nvim-treesitter/nvim-treesitter" }
+}
+
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins)
