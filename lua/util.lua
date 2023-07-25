513e1c85788e4f02cfc9ae8591803817309cf440
ntrupin <natrupin@gmail.com>
2023-07-25 10:57:06 -0400

    feat(util): find_buffer_by_name

---
+++ lua/util.lua
@@ -0,0 +1,17 @@
+-- util.lua
+-- misc utility functions
+
+local M = {}
+
+-- find buffer by name
+M.find_buffer_by_name = function(name)
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    local buf_name = vim.api.nvim_buf_get_name(buf)
+    if buf_name == name then
+      return buf
+    end
+  end
+  return -1
+end
+
+return M
