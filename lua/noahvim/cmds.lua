78dc121f2479a6b2cebb7ac19f86bf058652cd3f
ntrupin <natrupin@gmail.com>
2023-07-25 12:12:19 -0400

    feat(cmds): add git_menu

--- lua/noahvim/cmds.lua
+++ lua/noahvim/cmds.lua
@@ -3,6 +3,8 @@
 
 local M = {}
 
+local util = require("util")
+
 -- Noahvim menu
 M.noahvim_menu = function()
   require("noahvim.picker").create("Noahvim Menu", {
@@ -16,6 +18,21 @@ end
 
 vim.api.nvim_create_user_command("NoahvimMenu", M.noahvim_menu, { nargs = 0 })
 
+-- git menu + autocmd
+M.git_menu = function()
+  require("noahvim.picker").create("Noahvim Git Menu", {
+    { "Git Status", "Telescope git_status" },
+    { "Git Commits", "Telescope git_commits" },
+    { "Git Diff Buffer", "Git diffthis" },
+  }, require("telescope.themes").get_ivy({
+    layout_config = {
+      height = 10
+    }
+  }))
+end
+
+vim.api.nvim_create_user_command("NoahvimGit", M.git_menu, { nargs = 0 })
+
 -- create window helper
 M.create_window_from_file = function(filename)
   local keymapsdoc = vim.api.nvim_get_runtime_file(filename, false)[1]
@@ -26,12 +43,14 @@ end
 M.keymaps_window = function()
   M.create_window_from_file("noahvim-docs/noahvim-keymaps.md")
 end
+
 vim.api.nvim_create_user_command("NoahvimKeymaps", M.keymaps_window, { nargs = 0 })
 
 -- show info window + autocmd
 M.info_window = function()
   M.create_window_from_file("noahvim-docs/noahvim-info.md")
 end
+
 vim.api.nvim_create_user_command("NoahvimInfo", M.info_window, { nargs = 0 })
 
 return M
