5e948744b9b712ff8ccfeec85aa558fd03b72f59
ntrupin <natrupin@gmail.com>
2023-08-30 15:55:30 -0400

    feat(colorschemes): add index file

---
+++ lua/config/colorschemes/init.lua
@@ -0,0 +1,13 @@
+-- colorschemes/init.lua
+-- Colorscheme index
+
+local M = {}
+
+M.list = {
+  ["everforest"] = require("config.colorschemes.everforest"),
+  ["oxocarbon"] = require("config.colorschemes.oxocarbon"),
+  ["sonokai"] = require("config.colorschemes.sonokai"),
+  ["tokyonight"] = require("config.colorschemes.tokyonight")
+}
+
+return M
