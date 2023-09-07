f2b0e38b3aa616c0e7af5882cbe2cd3c2b79e25d
ntrupin <natrupin@gmail.com>
2023-09-07 14:50:13 -0400

    feat(colorschemes): patch oxocarbon comment font color

--- lua/config/colorschemes/oxocarbon.lua
+++ lua/config/colorschemes/oxocarbon.lua
@@ -6,6 +6,13 @@ local M = {}
 M.setup = function()
   vim.opt.background = "dark" -- set this to dark or light
   vim.cmd("colorscheme oxocarbon")
+
+  -- patch comment font
+  vim.api.nvim_set_hl(0, "Comment", {
+    fg = require("oxocarbon.colorutils")["blend-hex"]("#161616", "#ffffff", 0.6),
+    bg = "NONE",
+    italic = true
+  })
 end
 
 return M
