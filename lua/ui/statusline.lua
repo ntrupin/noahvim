64a656d4ab14b60c2c58af3de4c7e673266cacea
ntrupin <natrupin@gmail.com>
2023-09-08 11:07:32 -0400

    feat: implement custom statusline

---
+++ lua/ui/statusline.lua
@@ -0,0 +1,54 @@
+-- statusline.lua
+-- My custom statusline
+
+Statusline = {}
+
+local components = require("ui.components")
+
+Statusline.highlights = require("util.highlights")
+
+Statusline.build = function()
+
+  local ficon, fcolor = components.file_icon(vim.bo.filetype)
+
+  return table.concat({
+    -- Mode
+    components.mode_color(),
+    components.mode(),
+
+    -- Git Status
+    "%#NoahvimGrey#",
+    components.git_status(),
+
+    -- folder, file name, and status
+    "%#NoahvimGrey# ",
+    -- File info
+    "  ",
+    "%{expand('%:~:.')}",
+    " %m",
+
+    -- Git
+    " %#NoahvimGrey#",
+
+    -- Spacer
+    "%#NoahvimDefaultBG#",
+    "%=",
+
+    -- File information
+    "%#NoahvimGrey#",
+    components.fenc(),
+    "%#" .. fcolor .. "#",
+    ficon,
+    " %Y ",
+
+    -- Location in the file
+    components.mode_color(),
+    " %P %l:%c " --> Line, column, and page percentage
+  })
+end
+
+Statusline.setup = function()
+  vim.opt.statusline = "%{%v:lua.Statusline.build()%}"
+end
+
+return Statusline
