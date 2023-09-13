c789f2b03cbb406d44e97697397e2dbc46c09b0e
ntrupin <natrupin@gmail.com>
2023-09-13 13:36:44 -0400

    feat(catppuccin): override colors

--- lua/config/colorschemes/catppuccin.lua
+++ lua/config/colorschemes/catppuccin.lua
@@ -4,6 +4,17 @@
 local M = {}
 
 M.setup = function()
+  require("catppuccin").setup({
+    flavour = "mocha",
+    color_overrides = {
+      mocha = {
+        base = "#000000",
+        mantle = "#000000",
+        crust = "#000000"
+      }
+    }
+  })
+
   vim.cmd("colorscheme catppuccin")
 end
 
